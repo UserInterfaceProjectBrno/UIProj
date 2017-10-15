554f12e0abe2f18a22bb773fe48a3840b8de8aa0
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-10-15 17:29:40 +0300

    ahmet

--- app/src/main/java/com/example/nasty/UIProject/order_main_menu.java
+++ app/src/main/java/com/example/nasty/UIProject/order_main_menu.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class order_main_menu extends AppCompatActivity {
 
     Button OrderMenuButton;
+    ImageButton chickButt;
+    ImageButton carrotButt;
+    ImageButton cupButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,39 @@ public class order_main_menu extends AppCompatActivity {
         setContentView(R.layout.activity_order_main_menu);
 
         OrderMenuButton = (Button) findViewById(R.id.OrderMenuButton);
-
+        chickButt = (ImageButton) findViewById(R.id.chickenButton);
+        carrotButt = (ImageButton) findViewById(R.id.carrotButton);
+        cupButt = (ImageButton) findViewById(R.id.cupButton);
 
         OrderMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(order_main_menu.this, login_page.class);
                 startActivity(intent);
+            }
+        });
+
+        chickButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentChick= new Intent(order_main_menu.this, login_page.class);
+                startActivity(intentChick);
+            }
+        });
+
+        carrotButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCarrot = new Intent(order_main_menu.this, login_page.class);
+                startActivity(intentCarrot);
+            }
+        });
+
+        cupButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCup = new Intent(order_main_menu.this, login_page.class);
+                startActivity(intentCup);
             }
         });
     }
