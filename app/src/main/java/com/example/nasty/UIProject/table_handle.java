0bfff674d44f45a29a2a6998b898a45e043b4739
monikarosa <monika.rosa7@gmail.com>
2017-10-22 21:36:35 +0200

    Monika Rosa

--- app/src/main/java/com/example/nasty/UIProject/table_handle.java
+++ app/src/main/java/com/example/nasty/UIProject/table_handle.java
@@ -51,6 +51,7 @@ public class table_handle extends AppCompatActivity {
         DeletePersonBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(number==1)
                 number--;
                 NumberOfPeople.setText(String.valueOf(number));
             }
