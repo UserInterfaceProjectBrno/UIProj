95c470ed1fab8b6f0295981efda713f535f33bc0
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-10-24 18:20:05 +0200

    Nikos445

--- app/src/main/java/com/example/nasty/UIProject/order_main_menu.java
+++ app/src/main/java/com/example/nasty/UIProject/order_main_menu.java
@@ -7,9 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class order_main_menu extends AppCompatActivity {
 
     Button OrderMenuButton;
+    Button Database_Reset;
     ImageButton chickButt;
     ImageButton carrotButt;
     ImageButton cupButt;
@@ -17,10 +24,27 @@ public class order_main_menu extends AppCompatActivity {
     ImageButton pizzaButt;
     ImageButton wineButt;
 
+    int Children = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_main_menu);
+
+        final FirebaseDatabase TableDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference TableRef = TableDatabase.getReference().child("Tables");
+        TableRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+               Children = (int) dataSnapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
         OrderMenuButton = (Button) findViewById(R.id.OrderMenuButton);
         chickButt = (ImageButton) findViewById(R.id.chickenButton);
@@ -29,6 +53,15 @@ public class order_main_menu extends AppCompatActivity {
         fishButt = (ImageButton) findViewById(R.id.fishButton);
         pizzaButt = (ImageButton) findViewById(R.id.pizzaButton);
         wineButt = (ImageButton) findViewById(R.id.wineButton);
+        Database_Reset = (Button) findViewById(R.id.Database_Reset);
+
+        Database_Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0;i<Children; i++)
+                    TableRef.child(Integer.toString(i)).child("Reserved").child("Yes").setValue("No");
+            }
+        });
 
         OrderMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
