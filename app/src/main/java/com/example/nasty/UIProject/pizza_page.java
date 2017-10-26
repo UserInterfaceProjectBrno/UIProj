5ef916c9c463ecdf9dc8628e3400b9a56ad24ee2
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-10-26 09:51:00 +0200

    Nikos445

---
+++ app/src/main/java/com/example/nasty/UIProject/pizza_page.java
@@ -0,0 +1,13 @@
+package com.example.nasty.UIProject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pizza_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_page);
+    }
+}
