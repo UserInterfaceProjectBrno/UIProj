015f60c987923f40cb124d4754b031207393a856
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-10-26 10:24:05 +0200

    Nikos445

---
+++ app/src/main/java/com/example/nasty/UIProject/meat_page.java
@@ -0,0 +1,13 @@
+package com.example.nasty.UIProject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class meat_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meat_page);
+    }
+}
