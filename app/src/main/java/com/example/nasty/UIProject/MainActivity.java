0fbe2351b0a7092335889d6fb4ff4a455231c7ca
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-10-28 01:16:07 +0200

    Nikos445

--- app/src/main/java/com/example/nasty/UIProject/MainActivity.java
+++ app/src/main/java/com/example/nasty/UIProject/MainActivity.java
@@ -20,8 +20,11 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+    ////////////////////////////////////////////////////////////////////////////////////
+        getFragmentManager().beginTransaction().replace(R.id.content_frame
+                , new Order_Fragment())
+                .commit();    //START FROM ORDER PAGE
+    ////////////////////////////////////////////////////////////////////////////////////
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
