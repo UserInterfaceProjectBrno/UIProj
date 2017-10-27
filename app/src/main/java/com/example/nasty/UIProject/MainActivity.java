12001333dc6ecad9ebc6a03833911a7b08e9068c
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-10-28 00:03:07 +0200

    Nikos445

--- app/src/main/java/com/example/nasty/UIProject/MainActivity.java
+++ app/src/main/java/com/example/nasty/UIProject/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.nasty.UIProject;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -25,14 +21,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -83,11 +71,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
         FragmentManager fragmentManager = getFragmentManager();
 
-        if (id == R.id.nav_login) {
-            fragmentManager.beginTransaction().replace(R.id.content_frame
-                                                        , new Login_Fragment())
-                                                        .commit();
-        } else if (id == R.id.nav_order) {
+        if (id == R.id.nav_order) {
             fragmentManager.beginTransaction().replace(R.id.content_frame
                     , new Order_Fragment())
                     .commit();
