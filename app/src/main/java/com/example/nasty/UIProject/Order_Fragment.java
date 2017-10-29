7ed77cfd4b48e6cfd25242fcbe96fcdfd05465de
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-10-29 21:00:36 +0100

    Nikos445

--- app/src/main/java/com/example/nasty/UIProject/Order_Fragment.java
+++ app/src/main/java/com/example/nasty/UIProject/Order_Fragment.java
@@ -1,9 +1,7 @@
 package com.example.nasty.UIProject;
 
-import com.example.nasty.UIProject.Cart.*;
 import android.app.Fragment;
 import android.content.Context;
-import android.media.Image;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
@@ -12,28 +10,42 @@ import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
-import android.widget.ImageButton;
-
-import java.sql.Time;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import android.widget.TextView;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.locks.Lock;
 
 public class Order_Fragment extends Fragment {
     View Mview;
 
     Button FishButton;
     Button MealButton;
+    Button SaladButt;
+    Button PizzaButt;
+    Button AlcoholButt;
+    Button OtherDrink;
+
+    TextView LockStatus;
 
     TelephonyManager mngr;
     String imei="null";
 
+    String LockedState;
 
+    Button LockOrder;
+
+    FirebaseDatabase OrderDatabase = FirebaseDatabase.getInstance();
+    final DatabaseReference OrderRef = OrderDatabase.getReference().child("Orders");
 
 
     @Nullable
@@ -41,7 +53,62 @@ public class Order_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Mview = inflater.inflate(R.layout.order_activity, container, false);
 
-        FishButton = (Button) Mview.findViewById(R.id.FishButt);
+        /////////////////////////////////IMEI PERMISSION///////////////////////////////////////////
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{"android.permission.READ_PHONE_STATE"},
+                1);
+        mngr = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+         imei = mngr.getDeviceId();
+        /////////////////////////////////Initialization of IDs //////////////////////////////////////////////
+
+        FishButton  = (Button)   Mview.findViewById(R.id.FishButt);
+        LockOrder   = (Button)   Mview.findViewById(R.id.LockOrderButt);
+        AlcoholButt = (Button)   Mview.findViewById(R.id.AlcoholButt);
+        PizzaButt   = (Button)   Mview.findViewById(R.id.PizzaButt);
+        SaladButt   = (Button)   Mview.findViewById(R.id.SaladButt);
+        OtherDrink  = (Button)   Mview.findViewById(R.id.OtherDrinkButt);
+        MealButton  = (Button)   Mview.findViewById(R.id.MealButt);
+        LockStatus  = (TextView) Mview.findViewById(R.id.LockStatus);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        OrderRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                LockedState = dataSnapshot.child(imei).child("Locked").getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        LockOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderRef.child(imei).child("Locked").setValue("Yes");
+                fadeOutAndHideImage(FishButton);
+                fadeOutAndHideImage(LockOrder);
+                fadeOutAndHideImage(AlcoholButt);
+                fadeOutAndHideImage(PizzaButt);
+                fadeOutAndHideImage(SaladButt);
+                fadeOutAndHideImage(OtherDrink);
+                fadeOutAndHideImage(MealButton);
+                fadeIn(LockStatus);
+            }
+        });
+
+        if(LockedState == "Yes")
+        {
+            fadeOutAndHideImage(FishButton);
+            fadeOutAndHideImage(LockOrder);
+            fadeOutAndHideImage(AlcoholButt);
+            fadeOutAndHideImage(PizzaButt);
+            fadeOutAndHideImage(SaladButt);
+            fadeOutAndHideImage(OtherDrink);
+            fadeOutAndHideImage(MealButton);
+            fadeIn(LockStatus);
+        }
 
         FishButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +118,49 @@ public class Order_Fragment extends Fragment {
                         .commit();
             }
         });
+
+
+
+
         return Mview;
+    }
+    public void fadeOutAndHideImage(final View img) {
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(500);
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationEnd(Animation animation) {
+                img.setVisibility(View.GONE);
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationStart(Animation animation) {
+            }
+        });
+
+        img.startAnimation(fadeOut);
+    }
+
+    public void fadeIn(final View img) {
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator());
+        fadeIn.setDuration(400);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationEnd(Animation animation) {
+                img.setVisibility(View.VISIBLE);
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationStart(Animation animation) {
+            }
+        });
+
+        img.startAnimation(fadeIn);
     }
 }
