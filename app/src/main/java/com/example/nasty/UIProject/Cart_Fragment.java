c70ec4182f26b65280474eef17bdfa7493c62563
UserInterfaceProjectBrno <userinterfaceprojectbrno@gmail.com>
2017-11-04 22:31:54 +0100

    nikos445

--- app/src/main/java/com/example/nasty/UIProject/Cart_Fragment.java
+++ app/src/main/java/com/example/nasty/UIProject/Cart_Fragment.java
@@ -6,19 +6,38 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Cart_Fragment extends Fragment {
-    View Mview;
+    static FirebaseDatabase OrderDatabase = FirebaseDatabase.getInstance();
+    final static DatabaseReference OrderRef = OrderDatabase.getReference().child("Orders");
     public String data;
+    View Mview;
+    TextView CartText;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Mview = inflater.inflate(R.layout.activity_cart, container, false);
+        CartText = (TextView) Mview.findViewById(R.id.CartText);
 
+        OrderRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                CartText.setText(dataSnapshot.getValue().toString());
+            }
 
-        Toast.makeText(Mview.getContext().getApplicationContext(),data,Toast.LENGTH_LONG).show();
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
 
         return Mview;
     }
